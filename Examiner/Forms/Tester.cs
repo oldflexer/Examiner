a27fc90d8a37578834064a0a8e6e16dba1b23755
oldflexer <imielientiev@gmail.com>
2022-05-13 14:53:31 +0500

    Tester bPrev&bNext decision

--- Examiner/Forms/Tester.cs
+++ Examiner/Forms/Tester.cs
@@ -137,11 +137,11 @@ namespace Examiner.Forms
             // Отчищаем текущие ответы на вопрос
             _answers[_currentIndex].Clear();
             
-            // Сохраняем ответы со страницы экзаменатора
-            foreach (var cb in _checkBoxes.Where(cb => cb.Checked))
-            {
-                _answers[_currentIndex].Add(Convert.ToInt32(cb.Tag));
-            }
+            // // Сохраняем ответы со страницы экзаменатора
+            // foreach (var cb in _checkBoxes.Where(cb => cb.Checked))
+            // {
+            //     _answers[_currentIndex].Add(Convert.ToInt32(cb.Tag));
+            // }
             
             // Если пользователь на первой странице, то выходим из функции
             if (_currentIndex == 0) return;
@@ -166,11 +166,11 @@ namespace Examiner.Forms
             // Отчищаем текущие ответы на вопрос
             _answers[_currentIndex].Clear();
             
-            // Сохраняем ответы со страницы экзаменатора
-            foreach (var cb in _checkBoxes.Where(cb => cb.Checked))
-            {
-                _answers[_currentIndex].Add(Convert.ToInt32(cb.Tag));
-            }
+            // // Сохраняем ответы со страницы экзаменатора
+            // foreach (var cb in _checkBoxes.Where(cb => cb.Checked))
+            // {
+            //     _answers[_currentIndex].Add(Convert.ToInt32(cb.Tag));
+            // }
             
             // Если пользователь на последней странице, то выходим из функции
             if (_currentIndex == _pages.Count - 1) return;
